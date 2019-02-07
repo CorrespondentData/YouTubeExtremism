3c923428d4764835a3c65a2f88da78216ff5725f
Olaf Meuwese <olafmeuwese@gmail.com>
2019-02-08 00:41:48 +0100

    Create package setup based on make
    Use Makefile to fix dependencies and install package
    with additional cleaning.
    
    This makes it easier and also allows to automatic run of tests in the
    future.
    
    Adjusted the readme accordingly

--- DataCollection/setup.py
+++ DataCollection/setup.py
@@ -1,18 +1,14 @@
 from setuptools import setup, find_packages
 
+with open("requirements.txt") as handle:
+    project_requirements = [line.strip() for line in handle.readlines()]
+
 setup(name="youtubecollector",
-      version=0.1,
+      version="0.1.0",
       description="Module for getting data from youtube",
       url="https://github.com/CorrespondentData/YouTubeExtremism",
-      author="The Correspondent",
+      author="De Correspondent",
       packages=find_packages('youtubecollector'),
-      package_dir={"": "src"},
-      install_requires=[
-          "pandas",
-          "numpy",
-          "youtube_dl",
-          "google-api-python-client",
-          "webvtt-py",
-          "jupyter",
-          "requests"
-      ])
+      install_requires=project_requirements,
+      python_requires='>=3'
+      )
