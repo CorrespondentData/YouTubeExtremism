8b1ddfc1525aa94012f4711d029cb4e16fc2e1fe
Olaf Meuwese <olafmeuwese@gmail.com>
2019-02-13 23:11:21 +0100

    Create test-setup with mocking the google client api
    Using a json for setting up the client for the youtube v3 service and
    secondly calling the the get_channels method returning a predefined
    json the public method can be tested instead of the private method
    that just do converting.
    
    This can be done for every entity

--- DataCollection/tests/test_channels.py
+++ DataCollection/tests/test_channels.py
@@ -1,38 +1,60 @@
 from unittest import TestCase
 
-from channels import _convert_to_channel, channel
-from utils_for_test import read_json_from_file
+import pandas as pd
+from googleapiclient.discovery import build
+from googleapiclient.http import HttpMockSequence
+
+from channels import get_channels, channel
+from utils_for_test import get_content_from_file
 
 
 class ChannelTest(TestCase):
 
-    def test_convert_to_channel_entity(self):
-        response = read_json_from_file("full_channel_response.json")
+    def test_get_full_channel(self):
+        expected = [
+            channel(channel_id='Some_ID', channel_title='The test channel',
+                    channel_description='The Official YouTube Channel for testing',
+                    channel_default_language='en', channel_country='US',
+                    channel_uploads='UU_8WUrPbi8clO6sWt_FDvuA', channel_viewcount='2640735',
+                    channel_commentcount='0', channel_subscribercount='9779', channel_videocount='258',
+                    channel_topic_ids=['topic1', 'topic2', 'topic3'],
+                    channel_topic_categories=['https://en.wikipedia.org/wiki/Society',
+                                              'https://en.wikipedia.org/wiki/Politics'],
+                    channel_branding_keywords='"Testing is fun", "More Testing"')
+        ]
+        service_json = get_content_from_file("youtube_service.json")
+        full_channel_response = get_content_from_file("full_channel_response.json")
+        url = HttpMockSequence([
+            ({'status': '200'}, service_json),
+            ({'status': '200'}, full_channel_response)
+        ])
+        channel_seed = pd.DataFrame([{"channel_id": "Some_ID"}])
 
-        expected = channel(channel_id='Some_ID', channel_title='The test channel',
-                           channel_description='The Official YouTube Channel for testing',
-                           channel_default_language='en', channel_country='US',
-                           channel_uploads='UU_8WUrPbi8clO6sWt_FDvuA', channel_viewcount='2640735',
-                           channel_commentcount='0', channel_subscribercount='9779', channel_videocount='258',
-                           channel_topic_ids=['topic1', 'topic2', 'topic3'],
-                           channel_topic_categories=['https://en.wikipedia.org/wiki/Society',
-                                                     'https://en.wikipedia.org/wiki/Politics'],
-                           channel_branding_keywords='"Testing is fun", "More Testing"')
+        client = build("youtube", "v3", http=url, developerKey="key")
+        actual = get_channels(channel_seed, client)
 
-        actual = _convert_to_channel(response)
         self.assertEqual(expected, actual)
 
-    def test_convert_minimal_response(self):
-        response = read_json_from_file("nullable_fields_channel_response.json")
+    def test_get_minimal_channel(self):
+        expected = [
+            channel(channel_id='Some_ID', channel_title='The test channel',
+                    channel_description='The Official YouTube Channel for testing',
+                    channel_default_language='not set', channel_country='not set',
+                    channel_uploads='', channel_viewcount='2640735',
+                    channel_commentcount='0', channel_subscribercount='9779', channel_videocount='258',
+                    channel_topic_ids="not set",
+                    channel_topic_categories="not set",
+                    channel_branding_keywords="not set")
+        ]
+        service_json = get_content_from_file("youtube_service.json")
+        minimal_channel_response = get_content_from_file("nullable_fields_channel_response.json")
+        url = HttpMockSequence([
+            ({'status': '200'}, service_json),
+            ({'status': '200'}, minimal_channel_response)
+        ])
+        channel_seed = pd.DataFrame([{"channel_id": "Some_ID"}])
 
-        expected = channel(channel_id='Some_ID', channel_title='The test channel',
-                           channel_description='The Official YouTube Channel for testing',
-                           channel_default_language='not set', channel_country='not set',
-                           channel_uploads='', channel_viewcount='2640735',
-                           channel_commentcount='0', channel_subscribercount='9779', channel_videocount='258',
-                           channel_topic_ids="not set",
-                           channel_topic_categories="not set",
-                           channel_branding_keywords="not set")
+        client = build("youtube", "v3", http=url, developerKey="key")
+        actual = get_channels(channel_seed, client)
 
-        actual = _convert_to_channel(response)
         self.assertEqual(expected, actual)
--- DataCollection/tests/utils_for_test.py
+++ DataCollection/tests/utils_for_test.py
@@ -6,6 +6,11 @@ def get_file_from_test_resource(filename):
     return join(split(__file__)[0], "resources", filename)
 
 
+def get_content_from_file(filename):
+    with open(get_file_from_test_resource(filename)) as handle:
+        return handle.read()
+
+
 def read_json_from_file(filename):
     with open(get_file_from_test_resource(filename)) as json_file:
-         return json.loads(json_file.read())
+        return json.loads(json_file.read())
